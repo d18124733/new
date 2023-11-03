f1cc5aba6dcf4aeef9795f2f9ec53bd7f55e9db8
dan fegan <dfegan11@gmail.com>
2023-11-03 16:06:23 +0000

    readonly

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ const express = require('express');
 const app = express();
 const db = require('./persistence');
 const getItems = require('./routes/getItems');
-const addItem = require('./routes/addItem');
-const updateItem = require('./routes/updateItem');
-const deleteItem = require('./routes/deleteItem');
+//const addItem = require('./routes/addItem');
+//const updateItem = require('./routes/updateItem');
+//const deleteItem = require('./routes/deleteItem');
 
 app.use(express.json());
 app.use(express.static(__dirname + '/static'));
 
 app.get('/items', getItems);
-app.post('/items', addItem);
-app.put('/items/:id', updateItem);
-app.delete('/items/:id', deleteItem);
+//app.post('/items', addItem);
+//app.put('/items/:id', updateItem);
+//app.delete('/items/:id', deleteItem);
 
 db.init().then(() => {
     app.listen(3000, () => console.log('Listening on port 3000'));
